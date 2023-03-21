f2703166447709abd9a13e84f052dd2e44f7008f
Udaykiran-k <udaykiran.kondamudi@wmcontractor.com>
2023-03-21 12:50:12 +0530

    env set file

--- generateEnv.js
+++ generateEnv.js
@@ -5,42 +5,28 @@ const fs = require('fs');
 // const colors = require('colors');
 // require('dotenv').load();
 // `environment.ts` file structure
-if(`${process.env.stage}` == "local"){
-    var targetPath = './src/environments/environment.ts';
+switch(`${process.env.stage}`){
+    case dev:
+        var targetPath = './src/environments/environment.dev.ts';
+        break;
+    case qa:
+        var targetPath = './src/environments/environment.qa.ts';
+        break;
+    case prod:
+        var targetPath = './src/environments/environment.prod.ts';
+        break;
+    default:
+        var targetPath = './src/environments/environment.ts';
+}
+    
 const envConfigFile = `export const environment = {
     production: "${process.env.IS_PRODUCTION || false}",
     environment: "${process.env.stage || "local"}",
     userName: "${process.env.USERNAME || "I am from Dynamic Environment :D"}",
     company: "${process.env.COMPANY || "I am from Dynamic Environment :D"}"
 }`;
-}
-else if(`${process.env.stage}` == "dev"){
-    var targetPath = './src/environments/environment.dev.ts';
-    const envConfigFile = `export const environment = {
-        production: "${process.env.IS_PRODUCTION || false}",
-        environment: "${process.env.stage || "local"}",
-        userName: "${process.env.USERNAME || "I am from Dynamic Environment :D"}",
-        company: "${process.env.COMPANY || "I am from Dynamic Environment :D"}"
-    }`;
-}
-else if(`${process.env.stage}` == "qa"){
-    var targetPath = './src/environments/environment.qa.ts';
-    const envConfigFile = `export const environment = {
-        production: "${process.env.IS_PRODUCTION || false}",
-        environment: "${process.env.stage || "local"}",
-        userName: "${process.env.USERNAME || "I am from Dynamic Environment :D"}",
-        company: "${process.env.COMPANY || "I am from Dynamic Environment :D"}"
-    }`;
-}
-else if(`${process.env.stage}` == "prod"){
-    var targetPath = './src/environments/environment.prod.ts';
-    const envConfigFile = `export const environment = {
-        production: "${process.env.IS_PRODUCTION || false}",
-        environment: "${process.env.stage || "local"}",
-        userName: "${process.env.USERNAME || "I am from Dynamic Environment :D"}",
-        company: "${process.env.COMPANY || "I am from Dynamic Environment :D"}"
-    }`;
-}
+
+
 fs.writeFile(targetPath, envConfigFile, function (err) {
    if (err) {
        throw console.error(err);
